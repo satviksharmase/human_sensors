e979629801b95a2f8e5ec2f06261671d5f4c10a4
satviksharmase <52960097+satviksharmase@users.noreply.github.com>
2019-09-16 12:52:56 +1000

    Add files via upload

---
+++ HCSR505motion.ino
@@ -0,0 +1,31 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(6, INPUT);
+  pinMode(5, INPUT);
+  pinMode(4, INPUT);
+  
+  
+}
+
+void loop() {
+  if (digitalRead(4) == HIGH) {
+    Serial.println("Active");
+  }
+  else {
+    Serial.println("Inactive");
+  }
+  if (digitalRead(5) == HIGH) {
+    Serial.println("Active");
+  }
+  else {
+    Serial.println("Inactive");
+  }
+  if (digitalRead(6) == HIGH) {
+    Serial.println("Active");
+  }
+  else {
+    Serial.println("Inactive");
+  }
+  delay(1000);
+}
